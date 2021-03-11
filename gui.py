06acc9aa2a1ad8cbc669c3ced0666aa4ecadb47a
davidholton <david.holton@outlook.com>
2021-03-11 14:27:51 -0500

    Basic button demo
    .kv file is implicitly loaded through hvn.kv because we defined the class as "HVNApp"
    
    The button resides in a FloatLayout.
    Note the pos_hint and size_hint for the button.

--- gui.py
+++ gui.py
@@ -1,27 +1,15 @@
-from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
 
-class Background(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+
+class HVNLayout(Widget):
     pass
 
-class Hvn_Gui(App):
 
+class HVNApp(App):
     def build(self):
-        btn = Button(text ="Generate", 
-                   font_size ="20sp", 
-                   background_color =(1, 1, 1, 1), 
-                   color =(1, 1, 1, 1),  
-                   size =(32, 32), 
-                   size_hint =(.2, .2), 
-                   pos =(300, 250))  
-        return btn
-        #return Background()
+        return HVNLayout()
 
 
-if __name__ == '__main__':
-    Hvn_Gui().run()
+if __name__ == "__main__":
+    HVNApp().run()
