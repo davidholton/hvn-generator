9295bb19d8bac7e7a9b94b2f5358429d294c6ff3
Abel Dawson <abeldattsun@gmail.com>
2021-03-13 21:29:21 -0500

    added buttons and callback for buttons

--- gui.py
+++ gui.py
@@ -1,15 +1,42 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
 
 
-class HVNLayout(Widget):
+class HVNLayout(FloatLayout):
     pass
-
 
 class HVNApp(App):
     def build(self):
-        return HVNLayout()
+        parent = Widget()
+        genButt = Button(text="Generate", size= (200, 100))
+        optButt = Button(text="Options", pos=(Window.width - 100, Window.height - 100))
+        background = HVNLayout()
+        background.size = Window.size
+        genButt.bind(on_release=self.generate)
+        optButt.bind(on_release=self.genOptions)
+        parent.add_widget(background)
+        parent.add_widget(genButt)
+        parent.add_widget(optButt)
+        return parent
 
+    def generate(self, ob):
+        print(Window.size)
+        generation()
+
+    def genOptions(self, obj):
+        print(Window.size)
+        optionGeneration()
+    
+
+def generation():
+    pass
+
+def optionGeneration():
+    pass
 
 if __name__ == "__main__":
     HVNApp().run()
+
