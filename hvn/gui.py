5fb88edcf5775c7aeec44842793e261842b8131a
Abel Dawson <abeldattsun@gmail.com>
2021-04-02 11:40:33 -0400

    added generation

--- hvn/gui.py
+++ hvn/gui.py
@@ -2,34 +2,67 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
-# import __init__ as hvn
+import __init__ as hvn
 
-# baseGen = {'name': "", 'race': "",
-#           'gender': "", 'profession': "",
-#           'health': "", 'ac': "",
-#           'str': "", 'dex': "", 'con': "",
-#           'int': "", 'wis': "", 'cha': "",
-#           'saves': "", 'skills': "",
-#           'features': "", 'attack': "",
-#           'treasure': "", 'phys': "",
-#           'traits': ""}
+
+class baseGen:
+    power_score = -1
+    race = ""
+    gender = ""
+    profession = ""
+    char_class = ""
+    profession = ""
+    level = 0
+    name = ""
+
+
+class optGen:
+    power_score = -1
+    race = ""
+    gender = ""
+    profession = "" 
+    charClass = ""  
+    profession = ""
 
 dropdown = DropDown()
+pwScore = Button(text='Power Score', size_hint=(None, None))
+
+# this function really needs to be refactored, but for now it works
 def gen():
-    print('hello')
+    if baseGen.power_score == -1:
+        baseGen.power_score = hvn.generate_power_score()
+    if baseGen.race == "":
+        baseGen.race = hvn.generate_race()
+    if baseGen.gender == "":
+        baseGen.gender = hvn.generate_gender()
+    if baseGen.profession == "":
+        baseGen.profession = hvn.generate_profession(baseGen.power_score)
+    if baseGen.char_class == "":
+        baseGen.char_class = hvn.generate_class()
+
+    baseGen.name = hvn.generate_full_name(baseGen.race, baseGen.gender)
+    baseGen.level = hvn.generate_level(baseGen.power_score)
+
+    #this is a bad way to reset it, but for now it works
+    baseGen.power_score = -1
+    baseGen.race = ""
+    baseGen.gender = ""
+    baseGen.profession = ""
+    baseGen.char_class = ""
+    baseGen.profession = ""
+    baseGen.level = 0
+    baseGen.name = ""
 
 
 def opt(self):
-    pwScore = Button(text='Power Score', size_hint=(None, None))
     for index in range(10):
         btn = Button(text='Value %d' % index, size_hint_y=None, height=44)
         btn.bind(on_release=lambda btn: dropdown.select(btn.text))
         dropdown.add_widget(btn)
-    pwScore.bind(on_release=dropdown.open)
-    self.add_widget(pwScore)
 
-    print('bye')
-    
+    pwScore.bind(on_release=dropdown.open)
+
+
 class HVNLayout(Screen):
     def genBtn(self):
         gen()
@@ -43,7 +76,7 @@ class HVNOption(Screen):
         gen()
 
     def optBtn(self):
-        opt(self)
+        pass
 
     def abScore(self):
         pass
@@ -57,7 +90,6 @@ class HVNGenerate(Screen):
         opt(self)
 
 
-
 class HVNApp(App):
     def build(self):
         sm = ScreenManager()
@@ -65,7 +97,6 @@ class HVNApp(App):
         sm.add_widget(HVNGenerate(name='generate'))
         sm.add_widget(HVNOption(name='option'))
 
-        # return HVNLayout()
         return sm
 
 
